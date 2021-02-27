8396b8a5513659e7cb50f1e0f60253ed031d6ab1
Wollgramm <bj.wollgramm@gmail.com>
2021-02-27 18:42:49 +0100

    initialer commit

--- Katas/Snail/SnailSolution.cs
+++ Katas/Snail/SnailSolution.cs
@@ -4,30 +4,46 @@ public class SnailSolution
 {
     public static int[] Snail(int[][] array)
     {
-        int laenge = array.Length - 1;
-        List<int> retValue = new List<int>();
+        List<int> ret = new List<int>();
+        int len = array.Length - 1;
+        int max = len * len;
 
-        int zyklus = 0
- 
-
-       for (int zyklus = 0; zyklus < laenge / 2; zyklus++)
+        if (array == null)
         {
-            for (int r = 0 + zyklus; r < laenge - zyklus; r++)
+            ret.Add(new int());
+            return ret.ToArray();
+        }
+
+        if (len == 0)
+        {
+            ret.Add(array[0][0]);
+            return ret.ToArray();
+        }
+        for (int zyklus = 0; zyklus < len; zyklus++)
+        {
+
+            //(1+Zyklus). Line
+            for (int i = 0 + zyklus; i <= len - zyklus; i++)
             {
-                retValue.Add(array[0 + zyklus][r]);
+                ret.Add(array[0 + zyklus][i]);
+            }
+
+            for (int i = 1 + zyklus; i < len - zyklus; i++)
+            {
+                ret.Add(array[i][len - zyklus]);
+            }
+
+            for (int i = len - zyklus; i >= 1 + zyklus; i--)
+            {
+                ret.Add(array[len - zyklus][i]);
+            }
+
+            for (int i = len - zyklus; i >= 1 + zyklus; i--)
+            {
+                ret.Add(array[i][0 + zyklus]);
             }
 
         }
-
-        zeile 0 + c reihe 0 + c bis n-1 - c
- 
-zeile 1: bis n-1  reihe n-1
- 
-zeile n-1: reihe n-1 bis 1
- 
-zeile 1 bis n-1 - 1 reihe 0
- 
-
-            return retValue.ToArray();
+        return ret.ToArray();
     }
 }
